522dd20b767fcd969b980359b448a69f8d69777a
TRẦN LÊ CHÁNH HẠNH <4501104069@student.hcmue.edu.vn>
2020-11-28 19:47:51 +0700

    Added Read/Write function.

---
+++ GraphTheory/Form1.Designer.cs
@@ -0,0 +1,122 @@
+﻿namespace GraphTheory
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.StatusLbl = new System.Windows.Forms.Label();
+            this.dinh = new System.Windows.Forms.Label();
+            this.mat = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(12, 12);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(293, 198);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(363, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Save Graph";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.writeGr_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(363, 55);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Open...";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.readGr_Click);
+            // 
+            // StatusLbl
+            // 
+            this.StatusLbl.AutoSize = true;
+            this.StatusLbl.Location = new System.Drawing.Point(12, 236);
+            this.StatusLbl.Name = "StatusLbl";
+            this.StatusLbl.Size = new System.Drawing.Size(0, 13);
+            this.StatusLbl.TabIndex = 3;
+            // 
+            // dinh
+            // 
+            this.dinh.AutoSize = true;
+            this.dinh.Location = new System.Drawing.Point(484, 21);
+            this.dinh.Name = "dinh";
+            this.dinh.Size = new System.Drawing.Size(50, 13);
+            this.dinh.TabIndex = 4;
+            this.dinh.Text = "Số đinh: ";
+            // 
+            // mat
+            // 
+            this.mat.AutoSize = true;
+            this.mat.Location = new System.Drawing.Point(484, 65);
+            this.mat.Name = "mat";
+            this.mat.Size = new System.Drawing.Size(50, 13);
+            this.mat.TabIndex = 5;
+            this.mat.Text = "Ma Trận:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.mat);
+            this.Controls.Add(this.dinh);
+            this.Controls.Add(this.StatusLbl);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.richTextBox1);
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label StatusLbl;
+        private System.Windows.Forms.Label dinh;
+        private System.Windows.Forms.Label mat;
+    }
+}
+
---
+++ GraphTheory/Form1.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+namespace GraphTheory
+{
+    public partial class Form1 : Form
+    {
+        public const int MAX = 100;
+        int sodinh;//so dinh do thi
+        int[,] a = new int[MAX, MAX]; //ma tran ke
+       
+        public Form1()
+        {
+            InitializeComponent();
+            Directory.CreateDirectory("Graphs");
+        }
+
+        private void writeGr_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                //Tạo đồ thị dựa trên richTextBox, tên đặt bằng thời gian tao.
+                StreamWriter sw = new StreamWriter(Application.StartupPath + "\\Graphs\\" + string.Format("{0:yyyy-MM-dd_hh-mm-ss-tt}.txt", DateTime.Now));
+                sw.WriteLine(richTextBox1.Text.Trim('\r', '\n'));
+                sw.Close();
+                StatusLbl.Text = "Tạo Graph thành công !";
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void readGr_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                //Đọc đồ thị và dán dữ liệu vào richTextBox.
+                using (OpenFileDialog ofd = new OpenFileDialog())
+                {
+                    if (ofd.ShowDialog() == DialogResult.OK)
+                    {
+                        richTextBox1.Text = File.ReadAllText(ofd.FileName);
+                        StreamReader sr = new StreamReader(ofd.FileName);
+                        sodinh = Convert.ToInt32(sr.ReadLine());
+                        string input = sr.ReadToEnd().Trim('\r', '\n');
+                        int i = 0; int j = 0;
+                        a = new int[sodinh, sodinh];
+                        foreach (var row in input.Split('\n'))
+                        {
+                            j = 0;
+                            foreach (var col in row.Trim().Split(' '))
+                            {
+                                a[i, j] = int.Parse(col.Trim());
+                                j++;
+                            }
+                            i++;
+                        }
+                        dinh.Text += sodinh.ToString();
+                        mat.Text = "Ma Trận: " + "\n" + input;
+                    }
+                }
+                StatusLbl.Text = "Đọc file thành công. ";
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+}
